974e27617ab7a75df25ef8d1e21784264c7bc946
bhavyasasok <bhavyasasok8@gmail.com>
2025-03-07 20:25:21 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,74 @@
+<link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+ <link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+ <script
+src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+ .card{margin:15px;border-radius:15px; }
+img{height:180px;} spa{f ont-weight:bolder;
+font-size: large;} #visitsite{textdecoration:none; color:white;}
+ .card-text{ font-weight:bolder; font-size: large;}
+</style>
+<nav class="navbar navbar-expand-lg navbar-dark btn-dark">
+ <a class="navbar-brand" href="#">Ecommerce Price Comparison website Using Web
+Scrapping</a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse"
+datatarget="#basicExampleNav"
+ aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+ </button>
+ <div class="collapse navbar-collapse" id="basicExampleNav">
+ <ul class="navbar-nav mr-auto">
+<a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+aria-haspopup="true" aria-expanded="false">Dropdown</a> 
+<div class="dropdown-menu dropdown-dark"
+arialabelledby="navbarDropdownMenuLink">
+ <a class="dropdown-item" href="#">Action</a>
+ <a class="dropdown-item" href="#">Another action</a>
+ <a class="dropdown-item" href="#">Something else here</a>
+ </div>
+ </li-->
+ </ul>
+ <form class="form-inline">
+ <div class="md-form my-0">
+ <input class="form-control mr-sm-2" type="text" placeholder="Search"
+arialabel="Search">
+ </div>
+ </form>
+ </div>
+</nav>
+<br><br>
+<div class="jumbotron jumbotron-fluid">
+ <div class="container">
+ <h1>Ecommerce Price Comparison website Using Web Scrapping</h1>
+ <p>Price Comparison Tool : Here you can compare prices of a product on various ecommerce
+platforms..</p>
+ </div>
+</div>
+<footer class="page-footer font-small pt-4 btn-dark" >
+ <div class="container-fluid text-center text-md-left">
+ <div class="row">
+ <div class="col-md-6 mt-md-0 mt-3">
+ <h5 class="text-uppercase">Price Comparison Tool</h5>
+ <p>Here you can compare prices of a product on various e-commerce platforms.</p>
+ </div>
+ <hr class="clearfix w-100 d-md-none pb-3"> 
+<div class="col-md-3 mb-md-0 mb-3">
+ <h5 class="text-uppercase">E-commerce sites</h5>
+ <ul class="list-unstyled">
+ <li><a href="#!">Amazon</a></li>
+ <li> <a href="#!">Flipkart</a></li>
+ </ul></div>
+ <div class="col-md-3 mb-md-0 mb-3">
+ <h5 class="text-uppercase">Links</h5>
+ </div></div></div>
+</footer>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+ <script type="text/javascript" src="js/popper.min.js"></script>
+ <script type="text/javascript" src="js/bootstrap.min.js"></script>
+ <script type="text/javascript" src="js/mdb.min.js"></script>
+ <script type="text/javascript"></script>
